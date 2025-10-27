da4dbb976b1f75ac831166ad529f28f4a88c31c2
Banman03 <mckeownbaenan123@gmail.com>
2025-10-27 13:42:09 -0500

    removed libcurl files so that we can use boost::beast

--- gamma-connection/curl-utils/curl_ws_init.cpp
+++
@@ -1,8 +0,0 @@
-#include <string>;
-#include <curl/curl.h>;
-#include "constants.hpp";
-
-void curl_ws_init(CURL* handler, CURLcode* response) {
-    std::string ws_url = constants::WEBSOCKET_URL;
-
-}
--- gamma-connection/main.cpp
+++ gamma-connection/main.cpp
@@ -4,14 +4,6 @@
 #include "curl_utils.hpp";
 
 int main(int argc, char** argv) {
-    CURL* curl_handler_main;
-    CURLcode* curl_setup_response_main;
-
-    curl_init(curl_handler_main, curl_setup_response_main);
-
-    curl_websocket_init(curl_handler_main, curl_setup_response_main);
-    
-    curl_destroy(curl_handler_main);
 
     return 0;
 }
